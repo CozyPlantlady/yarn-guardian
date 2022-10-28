761c2658b015a2e6b43fd7d6ecf8bda8868ac224
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-10-28 09:02:42 +0000

    Create Producer model

--- stash/models.py
+++ stash/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Producer(models.Model):
+    producer = models.CharField(max_length=30)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.producer
