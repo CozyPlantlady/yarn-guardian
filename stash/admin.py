8ce6c613282fbe0c5b33eda7138016d35291886e
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-10-28 09:19:31 +0000

    Test Summernotes works

--- stash/admin.py
+++ stash/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Producer
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Producer)
+
+@admin.register(Producer)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('body')
--- yarn_guardian/urls.py
+++ yarn_guardian/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from stash.views import testing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', testing, name='testing')
+    path('', testing, name='testing'),
+    path('summernote/', include('django_summernote.urls')),
 ]
