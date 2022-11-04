66ef7b3f84825b07c0aeeedcd3f976ced09cbf80
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-04 09:24:38 +0000

    Add function to edit yarns

--- stash/views.py
+++ stash/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from .models import Producer, Yarn, Material, Color, Weight, Amount, User
 from .forms import AddYarnForm
@@ -26,3 +26,19 @@ def add_yarn(request):
             form.save()
         return redirect('get_stash')
     return render(request, 'stash/add_yarn.html', {'form': AddYarnForm})
+
+
+def edit_yarn(request, yarn_id):
+    yarn = get_object_or_404(Yarn, id=yarn_id)
+    if request.POST:
+        form = AddYarnForm(request.POST, instance=yarn)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+        return redirect('get_stash')
+    form = AddYarnForm(instance=yarn)
+    context = {
+        'form': form
+    }
+    return render(request, 'stash/edit_yarn.html', context, )
