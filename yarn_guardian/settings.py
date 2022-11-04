178eeceb916c45acb8a6733fe72ba6ae9ab0ba9f
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-04 10:15:22 +0000

    Add correct DATABASE in Settings

--- yarn_guardian/settings.py
+++ yarn_guardian/settings.py
@@ -95,18 +95,17 @@ WSGI_APPLICATION = 'yarn_guardian.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# Used in Heroku
-# DATABASES = {
-#  'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
-
-# Used when testing
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
