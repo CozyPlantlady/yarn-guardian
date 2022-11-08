d2e3f68c6a67c69635ff68d1e8633814f893d85b
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-08 19:46:53 +0000

    Add tests to test_views.py

---
+++ stash/test_views.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from .models import Yarn
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+
+
+class TestViews(TestCase, LoginRequiredMixin):
+
+    def setUp(self):
+        test_user1 = User.objects.create_user(
+            id='9',
+            username='testuser1',
+            email='',
+            password='workaholic!?/', )
+        test_user1.save()
+
+        test_yarn = Yarn.objects.create(
+            id='1',
+            name='Yarn name',
+            body='',
+            user=test_user1,
+            color_group='',
+            color_name='',
+            color_code='1',
+            amount='1',
+            weight='',
+            material='',
+            favorite='True',
+            frogged='False', )
+        test_yarn.save()
+
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stash/index.html')
+
+    def test_user_page(self):
+        response = self.client.get('/user_page')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'stash/user_page.html')
+
+#    def test_get_stash(self):
+#        response = self.client.get()
+#        response = self.client.get('/get_stash')
+#        self.assertEqual(response.status_code, 200)
+#        self.assertTemplateUsed(response, 'stash/stash_board.html')
