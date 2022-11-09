db62fa7a7f9a6b8507d150285d14c3c9732147ff
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-09 20:05:42 +0000

    Updated yarnModal with correct code

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -1,4 +1,4 @@
-/* modal element */
+/* modal element 
 const yarnModal = document.getElementById('yarnModal');
 yarnModal.addEventListener('show.bs.modal', (e) => {
   // Button that triggered the modal
@@ -17,4 +17,18 @@ yarnModal.addEventListener('show.bs.modal', (e) => {
 
   modalTitle.textContent = `${yarnName} yarn from ${yarnProducer}`;
   modalBody.textContent = `Notes about yarn: ${yarnBody}`;
+})
+*/
+
+
+$('#yarnModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var yarnName = button.data('yarn-name') // Extract info from data-* attributes
+  var yarnBody = button.data('yarn-body')
+
+
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + yarnName)
+  modal.find('.modal-body').text('Notes: ' + yarnBody)
+  modal.find('.modal-body input').val(yarnName)
 })
