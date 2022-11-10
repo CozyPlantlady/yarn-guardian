62d674a659cfedb9adc6c218002b84a92a769995
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-10 20:22:09 +0000

    Clean up myscripts.js file

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -1,26 +1,4 @@
-/* modal element 
-const yarnModal = document.getElementById('yarnModal');
-yarnModal.addEventListener('show.bs.modal', (e) => {
-  // Button that triggered the modal
-  const button = e.relatedTarget;
-  // Extract info from data-mdb-* attributes
-  const yarnName = button.getAttribute('data-bs-yarn-name');
-  const yarnBody = button.getAttribute('data-bs-yarn-body');
-  const yarnProducer = button.getAttribute('data-bs-yarn-producer');
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  const modalTitle = yarnModal.querySelector('.modal-title');
-  const modalBody = yarnModal.querySelector('.modal-body');
-  const modalBodyInput = yarnModal.querySelector('.modal-body input');
-
-  modalTitle.textContent = `${yarnName} yarn from ${yarnProducer}`;
-  modalBody.textContent = `Notes about yarn: ${yarnBody}`;
-})
-*/
-
-
+// Modal code from Bootstrap
 $('#yarnModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var yarnName = button.data('yarn-name') // Extract info from data-* attributes
