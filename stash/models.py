3b5d53235603a13f49cf42ea3164ee05c9ea3710
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-11 12:56:59 +0000

    Update Yarn method

--- stash/models.py
+++ stash/models.py
@@ -8,17 +8,17 @@ class Yarn(models.Model):
     body = models.TextField(blank=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, default='')
-    producer = models.CharField(max_length=50, null=False, blank=False)
+    producer = models.CharField(max_length=50)
 
     color_group = models.CharField(
-        max_length=10, null=True, blank=True, default='')
+        max_length=10, blank=True, default='')
     color_name = models.CharField(
-        max_length=50, null=True, blank=True, default='')
+        max_length=50, blank=True, default='')
     color_code = models.CharField(
-        max_length=6, null=True, blank=True, default='')
+        max_length=6, blank=True, default='')
 
     amount = models.IntegerField(null=True, blank=True, default='')
-    weight = models.CharField(max_length=10, null=True, blank=True, default='')
+    weight = models.CharField(max_length=10, blank=True, default='')
     material = models.CharField(max_length=100, null=True, blank=True)
     favorite = models.BooleanField(blank=True, default=False)
     frogged = models.BooleanField(blank=True, default=False)
