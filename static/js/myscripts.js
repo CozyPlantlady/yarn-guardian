b01f49369308bdbc4dfac916d90da13606584aa7
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-13 12:50:30 +0000

    Design project-board and modal

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -10,3 +10,16 @@ $('#yarnModal').on('show.bs.modal', function (event) {
   modal.find('.modal-body').text(yarnBody)
   modal.find('.modal-body input').val(yarnName)
 })
+
+
+$('#projectModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var projectName = button.data('project-name') // Extract info from data-* attributes
+  var projectBody = button.data('project-body')
+
+
+  var modal = $(this)
+  modal.find('.modal-title').text('Notes about ' + projectName)
+  modal.find('.modal-body').text(projectBody)
+  modal.find('.modal-body input').val(projectName)
+})
