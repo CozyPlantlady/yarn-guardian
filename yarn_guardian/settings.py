a4d54296793114cb0508c349991fc71045288946
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-14 20:44:15 +0000

    Fix typo

--- yarn_guardian/settings.py
+++ yarn_guardian/settings.py
@@ -103,12 +103,12 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
