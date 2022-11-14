fbb289c891186be73386e1b0e35ac99b5a927da0
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-11-14 21:20:47 +0000

    Change content according to Lighthouse

--- yarn_guardian/settings.py
+++ yarn_guardian/settings.py
@@ -105,10 +105,10 @@ if 'DATABASE_URL' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
